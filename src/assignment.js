37f11987c2f9a46e3a05e323c3228351c41e4339
Maureen Pwol <106152682+Maureen-sketch@users.noreply.github.com>
2022-07-21 08:39:33 +0000

    Sum Numbers and count even numbers of an array

--- src/assignment.js
+++ src/assignment.js
@@ -14,10 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum = arrayOfNumbers.reduce(function (prev, curr) {
+   return prev + curr 
+  })
 
+    return sum;
+}
+ assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,9 +31,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const EvenNumbers = arrayOfNumbers.filter(number => number % 2 === 0).length
+
+  
+    return EvenNumbers ;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
